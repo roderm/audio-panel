96366bec3e1969cdf2d5afa28d130712c1a44497
Roman Odermatt <odermatt@aol.ch>
2018-08-22 23:31:53 +0200

    commands from JSON-RPC specification

--- api/websocket.go
+++ api/websocket.go
@@ -2,76 +2,79 @@ package api
 
 import (
 	"encoding/json"
-	"fmt"
 	"golang.org/x/net/websocket"
 	"io"
+	"log"
 )
 
-type Message struct {
-	RequestID string      `json:"requestId"`
-	Method    string      `json:"method"`
-	Action    string      `json:"action"`
-	Data      interface{} `json:"data"`
+type Request struct {
+	JsonRpc string      `json:"jsonrpc"`
+	Method  string      `json:"method"`
+	Params  interface{} `json:"params"`
+	Id      interface{} `json:"id"`
 }
 
 type Response struct {
-	RequestID string      `json:"requestId"`
-	ErrorCode int         `json:"error_code"`
-	Data      interface{} `json:"data"`
+	JsonRpc string      `json:"jsonrpc"`
+	Result  interface{} `json:"result"`
+	Error   Error       `json:"error"`
+	Id      interface{} `json:"id"`
+}
+type Error struct {
+	Code    int         `json:"code"`
+	Message string      `json:"message"`
+	Data    interface{} `json:"data"`
 }
 
 type WebsocketHandler struct {
 	clients []*websocket.Conn
-	routes  map[string]func(*Message) interface{}
-	subs    map[string]func(*Message) chan interface{}
+	routes  map[string]func(interface{}) interface{}
+	subs    map[string]func(interface{}) chan interface{}
 }
 
 func NewWebsocketHandler() *WebsocketHandler {
 	return &WebsocketHandler{
-		routes: make(map[string]func(*Message) interface{}),
-		subs:   make(map[string]func(*Message) chan interface{}),
+		routes: make(map[string]func(interface{}) interface{}),
+		subs:   make(map[string]func(interface{}) chan interface{}),
 	}
 }
 
-func (wh *WebsocketHandler) Add(action string, cb func(*Message) interface{}) {
+func (wh *WebsocketHandler) Add(action string, cb func(interface{}) interface{}) {
 	wh.routes[action] = cb
 }
-func (wh *WebsocketHandler) AddSubscription(action string, cb func(*Message) chan interface{}) {
+func (wh *WebsocketHandler) AddSubscription(action string, cb func(interface{}) chan interface{}) {
 	wh.subs[action] = cb
 }
-func (wh *WebsocketHandler) execRequest(m *Message, conn *websocket.Conn) {
-	fmt.Println(m)
-	if wh.routes[m.Action] != nil {
-		data := wh.routes[m.Action](m)
+func (wh *WebsocketHandler) execRequest(m *Request, conn *websocket.Conn) {
+	if wh.routes[m.Method] != nil {
+		data := wh.routes[m.Method](m.Params)
 		r := Response{
-			RequestID: m.RequestID,
-			ErrorCode: 0,
-			Data:      data,
+			Id:     m.Id,
+			Result: data,
 		}
 		wh.WriteToClient(r, conn)
-	} else if wh.subs[m.Action] != nil {
-		subChan := wh.subs[m.Action](m)
+	} else if wh.subs[m.Method] != nil {
+		subChan := wh.subs[m.Method](m)
 		for {
 			data := <-subChan
 			r := Response{
-				RequestID: m.RequestID,
-				ErrorCode: 0,
-				Data:      data,
+				Id:     m.Id,
+				Result: data,
 			}
 			wh.WriteToClient(r, conn)
 		}
 	} else {
-		fmt.Errorf("No action found")
+		log.Println("No action found")
 	}
 }
 func (wh *WebsocketHandler) WriteToClient(r Response, conn *websocket.Conn) {
 	json_str, err := json.Marshal(r)
 	if err != nil {
-		fmt.Errorf(err.Error())
+		log.Println(err.Error())
 	}
 	_, err = conn.Write(json_str)
 	if err != nil {
-		fmt.Errorf(err.Error())
+		log.Println(err.Error())
 	}
 }
 
@@ -84,35 +87,15 @@ func (wh *WebsocketHandler) Handle(conn *websocket.Conn) {
 			return
 		}
 		if err != nil {
-			fmt.Println(err.Error())
+			log.Println(err.Error())
 			continue
 		}
-		message := &Message{}
+		message := &Request{}
 		err = json.Unmarshal(msg[:n], message)
 		if err != nil {
-			fmt.Println(err.Error())
+			log.Println(err.Error())
 			continue
 		}
 		go wh.execRequest(message, conn)
 	}
 }
-
-/*
-func SubscribeDevice(dev device.IDevice) {
-	avrs = append(avrs, dev)
-	dev.OnUpdate(func() {
-		avr := dev.GetAvr()
-		avrJs, err := json.Marshal(avr)
-		if err != nil {
-			return
-		}
-		for _, cl := range clients {
-			_, err = cl.Write(avrJs)
-			if err != nil {
-				fmt.Errorf(err.Error())
-			}
-
-		}
-	})
-}
-*/
