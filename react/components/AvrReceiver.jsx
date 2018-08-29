75bb4d5bd3edb41c9d6c4f904202ffcbd4e937a8
Roman Odermatt <odermatt@aol.ch>
2018-08-30 01:17:21 +0200

    fix typo, update for frontend works

--- react/components/AvrReceiver.jsx
+++ react/components/AvrReceiver.jsx
@@ -5,61 +5,58 @@ import WebsocketApi from "../js/websocket.api"
 import 'rc-slider/assets/index.css';
 
 class SmartSlider extends React.Component {
-	constructor(props) {
-		super(props)
-		const { value } = this.props
-		this.state = { value }
+    constructor(props) {
+        super(props)
+        const { value } = this.props
+        this.state = { value }
     }
     componentWillReceiveProps(nextProps) {
-        this.setState({ value: nextProps.value})
-        this.forceUpdate()
+        this.setValue(nextProps.value)
+        this.render()
     }
-	setValue(e) {
-		this.setState({ value: e })
-	}
-	update() {
-		this.props.onUpdate(this.state)
-	}
-	render() {
-		return <Slider value={this.state.value} onChange={this.setValue.bind(this)} onAfterChange={this.update.bind(this)} />
-	}
-} 
+    setValue(e) {
+        this.setState({ value: e })
+    }
+    update() {
+        this.props.onUpdate(this.state)
+    }
+    render() {
+        return <Slider value={this.state.value} onChange={this.setValue.bind(this)} onAfterChange={this.update.bind(this)} />
+    }
+}
 class AvrZone extends React.Component {
     constructor(props) {
         super(props)
         this.state = props.zone
     }
     componentWillReceiveProps(nextProps) {
-        this.zone = nextProps.zone
-        this.forceUpdate()
+        this.state = nextProps.zone
+        this.render()
     }
     setVolume(e) {
         this.state.Volume = e
     }
-    updateVolume(e){
+    updateVolume(e) {
         let ws = new WebsocketApi("ws://localhost:3000/api")
-        ws.rx("set_volume", [{"Volume": e.value, "Zone": this.props.zoneId, "Device": this.props.devId}]).then(console.log)
+        ws.rx("set_volume", [{ "Volume": e.value, "Zone": this.props.zoneId, "Device": this.props.devId }]).then(console.log)
     }
-    render() {   
+    render() {
         console.log("render zone")
-        return(
-        <Segment>
-            <h4>{this.props.zone.Name}</h4>
-            <Grid>
-                <Grid.Row>
-                    <Grid.Column width={10}>
-                    <SmartSlider value={this.state.Volume} min={1} max={50} onUpdate={this.updateVolume.bind(this)}/>
-                    </Grid.Column>
-                    <Grid.Column width={3}>
-                    <Input value={this.state.Volume} />
-                    </Grid.Column>
-                    <Grid.Column width={3}>
-                    <Button toggle active={!this.state.Muted}><Icon name='volume off'/></Button>
-                    </Grid.Column>
-                </Grid.Row>
-            </Grid>
-            
-        </Segment>
+        return (
+            <Segment>
+                <h4>{this.props.zone.Name}</h4>
+                <Grid>
+                    <Grid.Row>
+                        <Grid.Column width={13}>
+                            <SmartSlider value={this.state.Volume} min={1} max={50} onUpdate={this.updateVolume.bind(this)} />
+                        </Grid.Column>
+                        <Grid.Column width={3}>
+                            <Button toggle active={!this.state.Muted}><Icon name='volume off' /></Button>
+                        </Grid.Column>
+                    </Grid.Row>
+                </Grid>
+
+            </Segment>
         )
     }
 }
@@ -70,19 +67,19 @@ class AvrReceiver extends React.Component {
     }
     componentWillReceiveProps(nextProps) {
         this.receiver = nextProps.receiver
-        this.forceUpdate()
+        this.render()
     }
     render() {
         var zones = this.receiver.Zones;
         let myZones = []
-        if(zones[0] != undefined){
-            myZones.push(<AvrZone key={0} zone={zones[0]} zoneId={0} devId={this.receiver.Id}/>)
+        if (zones[0] != undefined) {
+            myZones.push(<AvrZone key={0} zone={zones[0]} zoneId={0} devId={this.receiver.Id} />)
         }
-        if(zones[1] != undefined){
-            myZones.push(<AvrZone key={1} zone={zones[1]} zoneId={1} devId={this.receiver.Id}/>)
+        if (zones[1] != undefined) {
+            myZones.push(<AvrZone key={1} zone={zones[1]} zoneId={1} devId={this.receiver.Id} />)
         }
-        if(zones[2] != undefined){
-            myZones.push(<AvrZone key={2} zone={zones[2]} zoneId={2} devId={this.receiver.Id}/>)
+        if (zones[2] != undefined) {
+            myZones.push(<AvrZone key={2} zone={zones[2]} zoneId={2} devId={this.receiver.Id} />)
         }
         return (
             <div>
